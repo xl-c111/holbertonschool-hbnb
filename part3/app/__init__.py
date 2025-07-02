573e1115fe2a105405ad3242841617b1fee5e9ed
xl-c111 <10430@holbertonstudents.com>
2025-07-03 00:44:58 +1000

    modify app/__init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
+from config import DevelopmentConfig
 from app.api.v1.users import api as users_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.places import api as places_ns
@@ -23,3 +24,4 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
 
     bcrypt.init_app(app)
+    return app
