c4557b81ccadc7669dda64ed4687e2cee5326d6f
Wawa <chutaphat@hotmail.com>
2025-07-03 17:44:02 +1000

    fix init

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_restx import Api
-from flask_bcrypt import Bcrypt
 from config import DevelopmentConfig
 from flask_sqlalchemy import SQLAlchemy
 from app.api.v1.users import api as users_ns
@@ -9,7 +8,7 @@ from app.api.v1.places import api as places_ns
 from app.api.v1.amenities import api as amenities_ns
 
 
-bcrypt = Bcrypt()
+
 db = SQLAlchemy()
 
 
@@ -18,7 +17,6 @@ def create_app(config_class="config.DevelopmentConfig"):
     app.config.from_object(config_class)
 
     # initialize extensions
-    bcrypt.init_app(app)
     db.init_app(app)
 
     # register API namespace
