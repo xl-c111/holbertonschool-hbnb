55182a831068579603038b07b442ce091cbbdb33
xl-c111 <10430@holbertonstudents.com>
2025-07-05 04:06:11 +1000

    update config.py

--- part3/config.py
+++ part3/config.py
@@ -1,22 +1,21 @@
 import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'SQLALCHEMY_DATABASE_URI',
+        'mysql+mysqlconnector://root:1234@localhost/hbnb'
+    )
 
 
 config = {
     'development': DevelopmentConfig,
     'default': DevelopmentConfig
 }
-
-app = Flask(__name__)  # <-- Create the Flask app
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:1234@localhost/hbnb'
-db = SQLAlchemy(app)
