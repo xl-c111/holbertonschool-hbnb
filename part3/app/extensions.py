ca95bc9c2292ae97c22fc86c7b25d62b84db1cf0
Wawa <chutaphat@hotmail.com>
2025-07-10 19:31:17 +1000

    add: JWT manager and register API auth

--- part3/app/extensions.py
+++ part3/app/extensions.py
@@ -1,6 +1,9 @@
 # app/extensions.py
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import  JWTManager
+
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+jwt = JWTManager()
